6cb1cd4297779fc966c47fc1de8f7d8197433dbb
Gustavo Santos <gustavosalve09@gmail.com>
2021-12-08 21:59:12 -0300

    Alterações de CSS e js

--- Calculadora/calc.js
+++ Calculadora/calc.js
@@ -22,34 +22,33 @@ function somar(){
 function subtrair(){
     R = parseInt(n1)-parseInt(n2)
     console.log(R)
-    var h3 = document.createElement('h1');
-    var result = document.createTextNode("A subtração dos dois numeros é "+R)
-    h3.appendChild(result);
-    document.body.appendChild(h3); 
+    var h3 = document.createElement('h3'); // cria um elemento no html
+    var result = document.createTextNode("A subtração dos dois numeros é "+R) /// criando uma variavel com um texto
+    h3.appendChild(result);  //add um 'nó' na estrutura
+    document.getElementById("r").appendChild(h3); // onde será inserido
 }
 function dividir(){
-    R = n1/n2
-    var h3 = document.createElement('h1');
-    var result = document.createTextNode("A divisão dos dois numeros é "+R)
-    h3.appendChild(result);
-    document.body.appendChild(h3); 
+    R = n1/n2;
+    var h3 = document.createElement('h3'); // cria um elemento no html
+    var result = document.createTextNode("A divisão dos dois numeros é "+R) /// criando uma variavel com um texto
+    h3.appendChild(result);  //add um 'nó' na estrutura
+    document.getElementById("r").appendChild(h3); // onde será inserido
 }
 function multiplicar(){
     R = n1*n2
-    var h3 = document.createElement('h1');
-    var result = document.createTextNode("A multiplicação dos dois numeros é "+R)
-    h3.appendChild(result);
-    document.body.appendChild(h3); 
+    var h3 = document.createElement('h3'); // cria um elemento no html
+    var result = document.createTextNode("A multiplicação dos dois numeros é "+R) /// criando uma variavel com um texto
+    h3.appendChild(result);  //add um 'nó' na estrutura
+    document.getElementById("r").appendChild(h3); // onde será inserido 
 }
 
 
-document.getElementById("somar").removeEventListener("click",somar); 
-document.getElementById("subtrair").removeEventListener("click",subtrair); 
-document.getElementById("dividir").removeEventListener("click",dividir); 
-document.getElementById("multiplicar").removeEventListener("click",multiplicar); 
+function exibir(){
+    // não funcionou
+
+}
 
 
 
-
-
-/// postar no git
+/// PROBLEMAS: Seria melhor se houvesse uma fução exibir 
+/// o resultado poderia desaparecer apos um tempo. 
