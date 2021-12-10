bc479d1068510c087522c0f797f50239923d3a42
Gustavo Santos <gustavosalve09@gmail.com>
2021-12-10 11:28:11 -0300

    alterações no ultimo projeto

--- TrocaCor/sema.js
+++ TrocaCor/sema.js
@@ -3,8 +3,10 @@ const yellow = document.getElementById('yellow');
 const green = document.getElementById('green');
 const buttons = document.getElementById('buttons');
 let corIndex = 0;
+let intervalo = null;
 
 const controle = (event) => {           //
+    pararIntervalo(); // funçao para parar o loop quando clicar em outro botão 
     funcoes[event.target.id]();         //     Isso para
 }                                       //   chamar os botoes de forma dinamica
 buttons.addEventListener('click', controle); //
@@ -25,27 +27,27 @@ const mudarCor = () => { // uma arrow function retorna um valor mais facilmente,
 
 
 let funcoes = { // um objeto para chamar as funçoes de maneira mais dinamica
-vermelho(){
-    red.style.background = 'red';
-    yellow.style.background = '#b3a314';
-    green.style.background = 'green';
+// Em um objeto só é possivel definir uma arrow function dessa forma 
+'vermelho': () => { red.style.background = 'red'; 
+                    yellow.style.background = '#b3a314'; 
+                    green.style.background = 'green';
 },
-amarelo(){
-    red.style.background = '#660000';
-    yellow.style.background = 'yellow';
-    green.style.background = 'green';
+'amarelo': () => {  red.style.background = '#660000';
+                    yellow.style.background = 'yellow';
+                    green.style.background = 'green';
 
 },
-verde(){
-    
-    red.style.background = '#660000';
-    yellow.style.background = '#b3a314';
-    green.style.background = '#00FF00';
+'verde': () =>     {red.style.background = '#660000';
+                    yellow.style.background = '#b3a314';
+                    green.style.background = '#00FF00';
 },
-auto(){
-    setInterval(mudarCor, 1000);
+'auto':() =>   {
+              intervalo = setInterval(mudarCor, 1000); // vai armazenar um id do setinterval
 }
-}
-//funcoes.amarelo(); ou funcoes[amarelo] para executar uma função dentro de um objeto
 
-// também funciona assim -> funcoes['verde'](); 
+}
+//funcoes.amarelo(); ou funcoes['amarelo']() para executar uma função dentro de um objeto
+
+const pararIntervalo = () => {
+    clearInterval(intervalo); // clear vai parar o processo do id armazenado
+}
